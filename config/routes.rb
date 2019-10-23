a38aa801e44983b5e696e7947a184c56de7aa961
160ayumi <160ayumi@gmail.com>
2019-10-23 14:28:24 +0900

    rails g devise:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: 'toppages#index'
   get   'toppages'      =>  'toppages#index'     #ツイート一覧画面
   get   'toppages/new'  =>  'toppages#new'       #ツイート投稿画面
