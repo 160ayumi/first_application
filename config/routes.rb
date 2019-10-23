9f5e6f2a09d5716af457168ba42664f197150cb1
160ayumi <160ayumi@gmail.com>
2019-10-23 14:20:50 +0900

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+devise_for :users
+root to: 'toppages#index'
   get   'toppages'      =>  'toppages#index'     #ツイート一覧画面
   get   'toppages/new'  =>  'toppages#new'       #ツイート投稿画面
   post  'toppages'      =>  'toppages#create'    #ツイート投稿機能
