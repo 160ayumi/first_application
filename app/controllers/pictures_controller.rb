1f34c75b9226d130ed430f059d0bafd6238a8e73
160ayumi <160ayumi@gmail.com>
2019-11-29 01:20:40 +0900

    バリデーション

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,18 +9,13 @@ class PicturesController < ApplicationController
   
   def create
     @picture = Picture.create(picture_params)
-    # binding.pry
-    # if @picture.save
-    #   flash[:image] = "写真を登録しました"
+    if @picture.save!
       redirect_to root_path
-
-    # # インスタンスの保存に失敗した場合の処理
-    # else
-    #   render :new
-    # end
- 
-
+    else
+      render 'new'
+    end
   end
+  
   private
   
   def picture_params
