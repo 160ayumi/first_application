34f523ef319739fbf53599ab9c302c224b6eecd2
160ayumi <160ayumi@gmail.com>
2019-11-30 19:53:07 +0900

    編集した内容保存

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_params, only:[:edit, :update]
-  
+
   def index
     @picture = Picture.all
     @tweet= Tweet.order("created_at DESC").page(params[:page]).per(10)
@@ -19,7 +19,14 @@ class TweetsController < ApplicationController
   end
 
   def update
+    # if @tweet.user_id == current_user.id
+    #   @tweet.update(tweet_params)
+    # end
+
+    @tweet.update(tweet_params)
+    redirect_to root_path
   end
+
   private
   
   def tweet_params
