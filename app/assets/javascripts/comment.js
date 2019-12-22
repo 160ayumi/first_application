8127a1823301ab6d208d43da0167a93ce1fe902e
160ayumi <160ayumi@gmail.com>
2019-12-22 16:00:07 +0900

    コメント非同期

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,26 @@
+$(function(){
+  function buildHTML(comment){
+    var html = `<div class="contents_show__comments__comment">
+                ${comment.text}</div>`
+                return html;
+  }
+
+$('#new_comment').on('submit', function(e){
+  e.preventDefault()
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents_show__comments__title').append(html);      
+      $('form')[0].reset();
+    })
+  })
+});
