a9246d3c040963d44b5f74115501b411ba28ae89
160ayumi <160ayumi@gmail.com>
2020-01-22 16:28:02 +0900

    ボタン追加

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
   $('#calendar').fullCalendar({
+
+    header:{
+      left:'prev,next,today',
+      center:'title,eventListButton',
+      right:'month agendaWeek agendaDay'
+  },
+    buttonText: {
+      month:'月',
+      week: '週',
+      day:'日'
+  },
+    customButtons:{
+      eventListButton:{
+          text: 'イベントリスト',
+          click:function(){
+              alert('イベントリスト!');
+          }
+      }
+    }
+
   });
 });
